fb03b3eb46a6f6fa62286f0c4629c2470ecb1fa3
chera-mihiretu <cheramihiretucm@gmail.com>
2025-02-27 20:58:17 +0300

    rearranging front end

--- frontend/app/study_content_detail/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function page() {
-  return (
-    <div className="text-2xl flex justify-center items-center h-screen">
-      STUDY CONTENT PAGE
-    </div>
-  );
-}
